39da2f81b9338c26110884ffce5ddf1247e49270
Patrick Culligan <pjculligan@gmail.com>
2024-12-06 12:57:49 +0000

    Load Test

--- k6tests/perf1.js
+++ k6tests/perf1.js
@@ -3,8 +3,8 @@ import http from "k6/http";
 
 export let options = {
     stages: [
-        { duration: "1m", target: 20 },  // Ramp up to 20 users over 1 minute
-        { duration: "3m", target: 20 },  // Stay at 20 users for 3 minutes
+        { duration: "1m", target: 10 },  // Ramp up to 20 users over 1 minute
+        { duration: "1m", target: 10 },  // Stay at 20 users for 3 minutes
         { duration: "1m", target: 0 }    // Ramp down to 0 users over 1 minute
     ],
     thresholds: {
@@ -13,27 +13,7 @@ export let options = {
 };
 
 export default function () {
-    let res = http.get("import { check, sleep } from "k6";
-import http from "k6/http";
-
-export let options = {
-  stages: [
-    { duration: "1m", target: 20 },  // Ramp up to 20 users over 1 minute
-    { duration: "1m", target: 20 },  // Stay at 20 users for 3 minutes
-    { duration: "1m", target: 0 }    // Ramp down to 0 users over 1 minute
-  ],
-  thresholds: {
-    "http_req_duration": ["p(95) < 200"], // 95% of requests must be below 200ms
-  },
-};
-
-export default function () {
-    let res = http.get("https://yourwebsite.comhttps://bpcalulator-green-slot-dscydwgqh3eyfxf4.ukwest-01.azurewebsites.net/");
-  check(res, {
-    "is status 200": (r) => r.status === 200,
-  });
-  sleep(1);
-}");
+    let res = http.get("https://bpcalulator-green-slot-dscydwgqh3eyfxf4.ukwest-01.azurewebsites.net/");
     check(res, {
         "is status 200": (r) => r.status === 200,
     });
