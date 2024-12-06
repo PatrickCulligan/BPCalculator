ff5df5ce21ab661e5fc78ca98ba8b73401446e85
Patrick Culligan <pjculligan@gmail.com>
2024-12-06 11:08:50 +0000

    .

--- k6tests/perf2.js
+++ k6tests/perf2.js
@@ -1,92 +1,78 @@
 import { check, sleep } from "k6";
 import http from "k6/http";
 
-// Export an options object to configure how k6 will behave during test execution.
-//
-// See https://docs.k6.io/docs/options
-//
+// Export configuration for the K6 test, including stages and thresholds
 export let options = {
-
-  // Either specify vus + duration or stages
-  // vus: 20,
-  // duration: "3m",
-
-  // This stages configuration will ramp to 20 Virtual Users over a minute,
-  // maintain those 20 concurrent users for 1 minute
-  // then ramp down to 0 over a minute i.e. ramp-up pattern of "load"
-  stages: [
-    { duration: "1m", target: 20 },            // 1 new vu every 3 seconds
-    { duration: "1m", target: 20 },
-    { duration: "1m", target: 0 }             // 1 less vu every 3 seconds
-  ],
-  
-  // set a threshold at 100 ms request duration for 95th percentile
-  // request duration = time spent sending request, waiting for response, and receiving response
-  // aka "response time"
-  // the test will be marked as failed by threshold if the value is exceeded 
-  // i.e. 95% of request duration times should be < 100 ms
- 	thresholds: {
-    "http_req_duration": ["p(95) < 200"]
-  },
-
-  // Don't save the bodies of HTTP responses by default, for improved performance
-  // Can be overwritten by setting the `responseType` option to `text` or `binary` for individual requests
-  discardResponseBodies: false,
-  // overriden below for GET
-  
-  cloud: {
-    distribution: {
-      distributionLabel1: { loadZone: 'amazon:us:ashburn', percent: 50 },
-      distributionLabel2: { loadZone: 'amazon:ie:dublin', percent: 50 },
+    stages: [
+        { duration: "1m", target: 20 }, // Ramp up to 20 virtual users over 1 minute
+        { duration: "1m", target: 20 }, // Maintain 20 virtual users for 1 minute
+        { duration: "1m", target: 0 }   // Ramp down to 0 virtual users over 1 minute
+    ],
+    thresholds: {
+        "http_req_duration": ["p(95) < 200"], // Set a threshold to keep 95% of request durations under 200ms
     },
-  },
- 
+    discardResponseBodies: false,
+    cloud: {
+        distribution: {
+            distributionLabel1: { loadZone: 'amazon:us:ashburn', percent: 50 },
+            distributionLabel2: { loadZone: 'amazon:ie:dublin', percent: 50 },
+        },
+    },
 };
 
 /**
  * Get a random integer between `min` and `max`.
- * 
- * @param {number} min - min number
- * @param {number} max - max number
- * @return {number} a random integer
+ *
+ * @param {number} min - Minimum number
+ * @param {number} max - Maximum number
+ * @return {number} A random integer between min and max
  */
-
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Export a default function - this defines the entry point for your VUs,
-// similar to the main() function in many other languages.
-export default function() {
- 
-  // do an initial GET, force response body so that form can be subsequently submited
-  let res = http.get("https://bpcalulator-green-slot-dscydwgqh3eyfxf4.ukwest-01.azurewebsites.net/", {"responseType": "text"});
+// Default function where the VUs will start executing the script
+export default function () {
+    // Initial GET request to retrieve the form page
+    let res = http.get("https://bpcalulator-green-slot-dscydwgqh3eyfxf4.ukwest-01.azurewebsites.net/", { "responseType": "text" });
 
-  // cookies automatically handled i.e. cookies sent by server will be re-presented by the client in all subsequent requests
-  // until end of script
+    check(res, {
+        "is status 200": (r) => r.status === 200
+    });
 
-  check(res, {
-    "is status 200": (r) => r.status === 200
-  });
+    // Extract anti-forgery token from the response body
+    let doc = res.body;
+    let tokenMatch = doc.match(/name="__RequestVerificationToken" type="hidden" value="([^"]+)"/);
+    let token = tokenMatch ? tokenMatch[1] : null;
 
-  // now do a POST without breaking CSRF feature of ASP.Net Core i.e. include cookie and matching hidden
-  // field from result (__RequestVerificationToken)
+    // Ensure that the anti-forgery token was extracted correctly
+    check(token, {
+        "anti-forgery token extracted": (t) => t !== null
+    });
 
-  // now POST form with cookie set and hidden field set to anti forgery token etc. 
-  // POST with random data to prevent server cached response to POST, discard response body
-  res = res.submitForm({
-    fields: { Bmi_WeightStones: getRandomInt(5, 50).toString(), Bmi_WeightPounds: getRandomInt(0, 13).toString(), 
-              Bmi_HeightFeet: getRandomInt(4, 7).toString(), Bmi_HeightInches: getRandomInt(0, 11).toString()}
-  });
+    // POST the form with random systolic and diastolic values and the anti-forgery token
+    let postData = {
+        "BP.Systolic": getRandomInt(70, 190).toString(),
+        "BP.Diastolic": getRandomInt(40, 100).toString(),
+        "__RequestVerificationToken": token
+    };
 
-  check(res, {
-    "is status 200": (r) => r.status === 200
-  });
+    res = http.post("https://bpcalulator-green-slot-dscydwgqh3eyfxf4.ukwest-01.azurewebsites.net/", postData, {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    });
 
-  // "think" for 3 seconds
-  sleep(3);
+    // Check that the POST request was successful
+    check(res, {
+        "is status 200": (r) => r.status === 200,
+        "response contains Category": (r) => r.body.includes("Category")
+    });
+
+    // Think time of 3 seconds between iterations
+    sleep(3);
 }
 
-// to run on Docker:
-// docker pull grafana/k6                                
-// docker run -i grafana/k6 run - <perf2.js
+// To run this script locally:
+// docker pull grafana/k6
+// docker run -i grafana/k6 run - < perf2.js
