8f3f527dcbeb3a0e7dc64737d5d258fd1515e72e
Patrick Culligan <pjculligan@gmail.com>
2024-12-06 12:52:49 +0000

    Load test K6 script

--- k6tests/perf1.js
+++ k6tests/perf1.js
@@ -2,35 +2,40 @@ import { check, sleep } from "k6";
 import http from "k6/http";
 
 export let options = {
-    // Modified load pattern with fewer VUs
     stages: [
-        { duration: "1m", target: 10 },           // Ramp up to 10 VUs over a minute
-        { duration: "1m", target: 10 },           // Maintain 10 VUs for 1 minute
-        { duration: "1m", target: 0 }             // Ramp down to 0 over a minute
+        { duration: "1m", target: 20 },  // Ramp up to 20 users over 1 minute
+        { duration: "3m", target: 20 },  // Stay at 20 users for 3 minutes
+        { duration: "1m", target: 0 }    // Ramp down to 0 users over 1 minute
     ],
-
-    // Increased the request duration threshold to 200 ms to reduce failure rate
     thresholds: {
-        "http_req_duration": ["p(95) < 200"]      // 95% of requests should be below 200 ms
-    },
-
-    discardResponseBodies: true,
-
-    cloud: {
-        distribution: {
-            distributionLabel1: { loadZone: 'amazon:us:ashburn', percent: 50 },
-            distributionLabel2: { loadZone: 'amazon:ie:dublin', percent: 50 },
-        },
+        "http_req_duration": ["p(95) < 200"], // 95% of requests must be below 200ms
     },
 };
 
 export default function () {
-    let res = http.get("https://bpcalulator-green-slot-dscydwgqh3eyfxf4.ukwest-01.azurewebsites.net/");
+    let res = http.get("import { check, sleep } from "k6";
+import http from "k6/http";
 
+export let options = {
+  stages: [
+    { duration: "1m", target: 20 },  // Ramp up to 20 users over 1 minute
+    { duration: "1m", target: 20 },  // Stay at 20 users for 3 minutes
+    { duration: "1m", target: 0 }    // Ramp down to 0 users over 1 minute
+  ],
+  thresholds: {
+    "http_req_duration": ["p(95) < 200"], // 95% of requests must be below 200ms
+  },
+};
+
+export default function () {
+    let res = http.get("https://yourwebsite.comhttps://bpcalulator-green-slot-dscydwgqh3eyfxf4.ukwest-01.azurewebsites.net/");
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
+  sleep(1);
+}");
     check(res, {
-        "is status 200": (r) => r.status === 200
+        "is status 200": (r) => r.status === 200,
     });
-
-    // "Think" for 3 seconds to simulate real user behavior
-    sleep(3);
+    sleep(1);
 }
